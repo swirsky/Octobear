518183b438b4401a925236161c32e9c8bedcb48e
Eric Swirsky <barbaric.eric.s@gmail.com>
2015-03-08 19:58:00 -0400

    added prod url

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 ENV['DEV_URL'] = '0.0.0.0:3000'
-
+ENV['PROD_URL'] = 'http://rocky-anchorage-3250.herokuapp.com/'
