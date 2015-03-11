534359744d481bde0ab96b6e84bad36e78cdbf63
Eric Swirsky <barbaric.eric.s@gmail.com>
2015-03-10 22:14:18 -0400

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has a factory" do
+    expect(create(:user)).to be_valid
+  end
+
+  it "is invalid with bad email / password" do
+    [nil, "", 12, true, false, "bad"].each do |p|
+      u = User.new(email:p, password:"password")
+      expect(u.save).to eq false
+      u = User.new(email:"valid@test.com", password:p)
+      expect(u.save).to eq false
+    end
+  end
+
 end
