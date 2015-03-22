cda039a6030d10c027769d045d81b022b80b7946
Eric Swirsky <barbaric.eric.s@gmail.com>
2015-03-22 10:49:21 -0400

    display pages better

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,7 +7,7 @@ class Page < ActiveRecord::Base
   after_destroy :kill_ciphers
 
   def blockify_text
-    lines.join("<br>")
+    lines.join("<br>").gsub('\n', '<br>')
   end
 
   def get_line(line)
@@ -16,7 +16,36 @@ class Page < ActiveRecord::Base
 
   def lines
     #get first 50, then keep grabbing until space
-    self.text.scan(/.{#{self.line_length}}|.+[(a-zA-Z)\.\,\!]{1,5}/).each(&:strip!)
+    #self.text.scan(/.{#{self.line_length}}|.+[(a-zA-Z)\.\,\!]{1,5}/).each(&:strip!)
+    chars = self.text
+    lines = []
+    while !chars.empty?
+      if chars[0..50].match(/\n/)
+        t = chars[0..chars.index(/\n/)]
+      elsif chars.length >= 50
+        t = chars[0..50]
+        done = false
+        ex = ''
+        puts 'checking more'
+        chars[51..55].chars.each do |c|
+          next if done
+          puts 'loop'
+          if c.match(/[a-zA-Z\.\,\!\?]/)
+            ex += c
+            done = true if c.match(/[\.\,\!\?]/)
+          else
+            done = true
+          end
+        end
+        t += ex if done
+        puts t.inspect
+      else
+        t = chars
+      end
+    lines << t
+    chars.sub!(t, '')
+    end
+    lines
   end
 
   private
